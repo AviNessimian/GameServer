010a2a2883fb0c044dfe16553c2a8ae221216cb4
avi nessimian <nessimian.avi@outlook.com>
2023-11-05 00:17:01 +0200

    s

--- GameServer.Core/Bases/BaseResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GameServer.Core.Bases;
-
-public class BaseResponse
-{
-    public bool IsSuccess { get; set; }
-    public string Error { get; set; }
-}
--- GameServer.Core/Models/PlayerConnaction.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GameServer.Core.Models;
-
-public class PlayerConnaction
-{
-    public string PlayerId { get; set; }
-    public string ConnectionId { get; set; }
-}
